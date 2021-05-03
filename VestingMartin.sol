e701260beb4c104fcd9dd9ffa9dd91795db0bf4c
ashishk74 <53088472+ashishk74@users.noreply.github.com>
2021-05-03 22:49:26 +0530

    Added Arrays as parameter to addTokenGrant()

--- VestingMartin.sol
+++ VestingMartin.sol
@@ -135,6 +135,7 @@ contract VestingMartin is ERC20 {
     using SafeMath for uint256;
     using SafeMath for uint24;
     using SafeMath for uint16;
+    using SafeMath for uint8;
     
     ERC20 public token;
     
@@ -149,7 +150,7 @@ contract VestingMartin is ERC20 {
     }
 
     uint24 constant internal SECONDS_PER_DAY = 86400;
-    uint24 constant internal SECONDS_PER_MONTH = 2592000;
+//    uint24 constant internal SECONDS_PER_MONTH = 2592000;
     
     event GrantAdded(address indexed recipient, uint256 vestingId);
     event GrantTokensClaimed(address indexed recipient, uint256 amountClaimed);
@@ -163,7 +164,7 @@ contract VestingMartin is ERC20 {
         uint256 amount;
         uint16 vestingDuration; // In months
         uint16 vestingCliff;    // In months
-        uint16 secondsClaimed;
+        uint16 daysClaimed;
         uint256 totalClaimed;
         address recipient;
     }
@@ -177,7 +178,7 @@ contract VestingMartin is ERC20 {
 
     mapping (uint256 => Grant) public tokenGrants;
     mapping (address => uint[]) private activeGrants;
-    mapping (VGroup => VestingGroup) parameter;
+    mapping (VGroup => VestingGroup) parameter; // Enum mapped to Struct
     //address public admin;
     uint256 public totalVestingCount;
     
@@ -193,26 +194,35 @@ contract VestingMartin is ERC20 {
         parameter[_name] = VestingGroup(_vestingDurationInMonths, _vestingCliffInMonths, _percent);
     }
    
-    function addTokenGrant(address _recipient, VGroup _name, uint256 _startTime,
-                    uint256 _amount)external payable onlyAdmin{
+    function addTokenGrant(address[] memory _recipient, VGroup[] memory _name, 
+                uint256[] memory _startTime,uint256[] memory _amount)external payable onlyAdmin{
+    require(_recipient.length == _name.length, "Different array length");
+    require(_recipient.length == _startTime.length, "Different array length");
+    require(_recipient.length == _amount.length, "Different array length");
+    
+    for(uint i=0;i<_recipient.length;i++) {
+      require(_startTime[i] != 0, "should be positive");
+      uint256 amountVestedPerDay = _amount[i].div(parameter[_name[i]].vestingDuration.mul(30));
+      require(amountVestedPerDay > 0, "0-amount-vested-per-period");
 
         // Transfer the grant tokens under the control of the vesting contract
-        token.approve(owner_, _amount);
-        require(token.transferFrom(owner_, address(this), _amount), "transfer failed");
+        token.approve(owner_, _amount[i]);
+        require(token.transferFrom(owner_, address(this), _amount[i]), "transfer failed");
 
         Grant memory grant = Grant({
-            startTime: _startTime == 0 ? currentTime() : _startTime,
-            amount: _amount,
-            vestingDuration: parameter[_name].vestingDuration,
-            vestingCliff: parameter[_name].vestingCliff,
-            secondsClaimed: 0,
+            startTime: _startTime[i] == 0 ? currentTime() : _startTime[i],
+            amount: _amount[i],
+            vestingDuration: parameter[_name[i]].vestingDuration,
+            vestingCliff: parameter[_name[i]].vestingCliff,
+            daysClaimed: 0,
             totalClaimed: 0,
-            recipient: _recipient
+            recipient: _recipient[i]
         });
         tokenGrants[totalVestingCount] = grant;
-        activeGrants[_recipient].push(totalVestingCount);
-        emit GrantAdded(_recipient, totalVestingCount);
-        totalVestingCount++;
+        activeGrants[_recipient[i]].push(totalVestingCount);
+        emit GrantAdded(_recipient[i], totalVestingCount);
+        totalVestingCount++;    //grantId
+        }
     }
 
     function getActiveGrants(address _recipient) public view returns(uint256[] memory){
@@ -232,20 +242,20 @@ contract VestingMartin is ERC20 {
 
         // Check cliff was reached
         uint elapsedTime = currentTime().sub(tokenGrant.startTime);
-        //uint elapsedDays = elapsedTime.div(SECONDS_PER_DAY);
+        uint elapsedDays = elapsedTime.div(SECONDS_PER_DAY);
         
-        if (elapsedTime < tokenGrant.vestingCliff.mul(SECONDS_PER_MONTH)) {
-            return (uint16(elapsedTime), 0);
+        if (elapsedDays < tokenGrant.vestingCliff.mul(30)) {
+            return (uint16(elapsedDays), 0);
         }
 
         // If over vesting duration, all tokens vested
-        if (elapsedTime >= tokenGrant.vestingDuration.mul(SECONDS_PER_MONTH)) {
+        if (elapsedDays >= tokenGrant.vestingDuration.mul(30)) {
             uint256 remainingGrant = tokenGrant.amount.sub(tokenGrant.totalClaimed);
             return (tokenGrant.vestingDuration, remainingGrant);
         } else {
-            uint16 timeVested = uint16(elapsedTime.sub(tokenGrant.secondsClaimed));
-            uint256 amountVestedPerSecond = tokenGrant.amount.div(uint256(tokenGrant.vestingDuration.mul(SECONDS_PER_MONTH)));
-            uint256 amountVested = uint256(timeVested.mul(amountVestedPerSecond));
+            uint16 timeVested = uint16(elapsedDays.sub(tokenGrant.daysClaimed));
+            uint256 amountVestedPerDay = tokenGrant.amount.div(uint256(tokenGrant.vestingDuration.mul(30)));
+            uint256 amountVested = uint256(timeVested.mul(amountVestedPerDay));
             return (timeVested, amountVested);
         }
     }
@@ -259,7 +269,7 @@ contract VestingMartin is ERC20 {
         require(amountVested > 0, "amountVested is 0");
 
         Grant storage tokenGrant = tokenGrants[_grantId];
-        tokenGrant.secondsClaimed = uint16(tokenGrant.secondsClaimed.add(timeVested));
+        tokenGrant.daysClaimed = uint16(tokenGrant.daysClaimed.add(timeVested));
         tokenGrant.totalClaimed = uint256(tokenGrant.totalClaimed.add(amountVested));
         
         require(token.transfer(tokenGrant.recipient, amountVested), "no tokens");
@@ -289,7 +299,7 @@ contract VestingMartin is ERC20 {
         tokenGrant.amount = 0;
         tokenGrant.vestingDuration = 0;
         tokenGrant.vestingCliff = 0;
-        tokenGrant.secondsClaimed = 0;
+        tokenGrant.daysClaimed = 0;
         tokenGrant.totalClaimed = 0;
         tokenGrant.recipient = address(0);
 
@@ -300,9 +310,9 @@ contract VestingMartin is ERC20 {
         return block.timestamp;
     }
 
-    function tokensVestedPerSecond(uint256 _grantId) public view returns(uint256) {
+    function tokensVestedPerDay(uint256 _grantId) public view returns(uint256) {
         Grant storage tokenGrant = tokenGrants[_grantId];
-        return tokenGrant.amount.div(uint256(tokenGrant.vestingDuration.mul(SECONDS_PER_MONTH)));
+        return tokenGrant.amount.div(uint256(tokenGrant.vestingDuration.mul(30)));
     }
 
     function changeAdmin(address _newAdmin) 
